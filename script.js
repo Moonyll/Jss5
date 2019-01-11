4dc23855deae05f9788c1b3adaaf5ce9db7c0f99
Moonyll <s.pouwel@laposte.net>
2019-01-11 09:40:02 +0100

    Jss5

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿function chg() {
+    document.getElementById('image1').src = "assets/image1_2.jpg";
+    document.getElementById('image2').src = "assets/image2_2.jpg";
+    document.getElementById('image3').src = "assets/image3_2.jpg";
+    document.getElementById('image4').src = "assets/image4_2.jpg";
+    document.getElementById('image5').src = "assets/image5_2.jpg";
+}
